691c67da885d977f2f4abeec811011ee8e84ce0e
Jeon Insun <jeonis1018@gmail.com>
2025-07-17 23:59:01 +0900

    1000, 1001, 1085 저장 문제로 재push

--- Bronze/1000.c
+++ Bronze/1000.c
@@ -1,16 +1,5 @@
-#include <stdio.h>
-
-int main() {
-    int a, b;
-
-    scanf("%d %d", &a, &b);
-    printf("%d", a + b);
-
-    return 0;
-}
-
-/*
- */
+/* 백준 1000(A + B)
+두 값을 입력 받아서 두 값의 합을 출력 */
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -21,7 +10,10 @@ int main() {
 #include <stdbool.h>
 
 int main(void) {
+    int a, b;
 
+    scanf("%d %d", &a, &b);
+    printf("%d", a + b);
 
     return 0;
 }
