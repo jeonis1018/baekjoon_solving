b8f65a965356c2fe37e43ae72b1bc0abaae56236
jeonis1018 <jeonis1018@gmail.com>
2025-09-17 09:54:22 +0900

    작성 중 : 7662

--- Gold/7662.c
+++ Gold/7662.c
@@ -20,18 +20,71 @@ void init_queue (db_queue* dq, int size) {
     dq->heap[0] = 0;
 }
 
+void min_up () {
+
+}
+
+void max_up () {
+
+}
+
+void min_down () {
+
+}
+
+void max_down () {
+    
+}
+
 void insert (db_queue* dq, int value) {
     dq->rear++;
+    dq->heap[dq->rear] = value;
+    int parent = dq->rear / 2;
+    int grandparent = dq->rear / 4;
     if ((int)floor(log2(dq->rear)) % 2 == 0) {  //min 레벨에 삽입되었을 경우
-        if (dq->rear / 2 != 0 && dq->heap[dq->rear / 2] < value) {
-            
+        if (parent > 0) {
+            if (value > dq->heap[parent]) {
+                dq->heap[dq->rear] = dq->heap[parent];
+                grandparent /= 2;
+                while (grandparent > 0) {
+                    max_up();
+                }
+            } else {
+                while (grandparent > 0) {
+                    min_up();
+                }
+            }
         }
     } else {    //max레벨에 삽입되었을 경우
-
+        if (parent > 0) {
+            if (value < dq->heap[parent]) {
+                dq->heap[dq->rear] = dq->heap[parent];
+                grandparent /= 2;
+                while (grandparent > 0) {
+                    min_up();
+                }
+            } else {
+                while (grandparent > 0) {
+                    max_up();
+                }
+            }
+        }
     }
 }
 
 int delete_max (db_queue* dq) {
+     if (dq->rear <= 0) return 0;
+     else if (dq->rear == 1 || dq->rear == 2) return dq->heap[dq->rear--];
+     else {
+        if (dq->heap[2] >= dq->heap[3]) {
+            int result = dq->heap[2];
+            dq->heap[2] = dq->heap[dq->rear--];
+            if (dq->rear )
+        } else {
+            int result = dq->heap[3];
+            dq->heap[3] = dq->heap[dq->rear--];
+        }
+     }
 
 }
 
@@ -62,7 +115,6 @@ int main(void) {
             if (func == 'I') {
                 insert(&dq, value);
             } else if (func == 'D') {
-                if (dq.rear <= 0) continue;
                 if (value == 1) {
                     delete_max(&dq);
                 } else if (value == -1) {
