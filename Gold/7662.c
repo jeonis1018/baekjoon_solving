dcb00a0f3da75c785870214fe57f598b6931dc4d
jeonis1018 <jeonis1018@gmail.com>
2025-09-23 16:55:54 +0900

    number 1874

--- Gold/7662.c
+++ Gold/7662.c
@@ -10,129 +10,85 @@
 #include <stdbool.h>
 
 typedef struct {
-    int* heap;
+    int* tree;
     int rear;
-} db_queue;
+} heap;
 
-void init_queue (db_queue* dq, int size) {
-    dq->heap = malloc(size * sizeof(int));
-    dq->rear = 0;
-    dq->heap[0] = 0;
+typedef struct node {
+    int key;
+    int cnt;
+    node* next;
+} node;
+
+typedef struct {
+    int size;
+    node** table;
+} deletion_map;
+
+void init_map (deletion_map d_map) {
+    d_map.size = 131070;
+    d_map.table = calloc(d_map.size, sizeof(node*));
 }
 
-void min_up () {
-
+void insert_map (deletion_map d_map, int value) {
+    int index = (value % d_map.size + d_map.size) % d_map.size;
+    node nd;
+    nd.key = value;
 }
 
-void max_up () {
-
+void init_heap (heap h, int size) {
+    h.tree = malloc((size + 1) * sizeof(int));
+    h.tree[0] = 0;
+    h.rear = 0;
 }
 
-void min_down () {
-
-}
-
-void max_down () {
-    
-}
-
-void insert (db_queue* dq, int value) {
-    dq->rear++;
-    dq->heap[dq->rear] = value;
-    int parent = dq->rear / 2;
-    int grandparent = dq->rear / 4;
-    if ((int)floor(log2(dq->rear)) % 2 == 0) {  //min 레벨에 삽입되었을 경우
-        if (parent > 0) {
-            if (value > dq->heap[parent]) {
-                dq->heap[dq->rear] = dq->heap[parent];
-                grandparent /= 2;
-                while (grandparent > 0) {
-                    max_up();
-                }
-            } else {
-                while (grandparent > 0) {
-                    min_up();
-                }
-            }
-        }
-    } else {    //max레벨에 삽입되었을 경우
-        if (parent > 0) {
-            if (value < dq->heap[parent]) {
-                dq->heap[dq->rear] = dq->heap[parent];
-                grandparent /= 2;
-                while (grandparent > 0) {
-                    min_up();
-                }
-            } else {
-                while (grandparent > 0) {
-                    max_up();
-                }
-            }
-        }
-    }
-}
-
-int delete_max (db_queue* dq) {
-     if (dq->rear <= 0) return 0;
-     else if (dq->rear == 1 || dq->rear == 2) return dq->heap[dq->rear--];
-     else {
-        if (dq->heap[2] >= dq->heap[3]) {
-            int result = dq->heap[2];
-            dq->heap[2] = dq->heap[dq->rear--];
-            if (dq->rear )
-        } else {
-            int result = dq->heap[3];
-            dq->heap[3] = dq->heap[dq->rear--];
-        }
-     }
+void insert_min (heap min, int value) {
 
 }
 
-int delete_min(db_queue* dq) {
+void insert_max (heap max, int value) {
 
 }
 
-void free_queue (db_queue* dq) {
-    free(dq->heap);
+int delete_min (heap min, deletion_map map) {
+
+}
+
+int delete_max (heap max, deletion_map map) {
+
 }
 
 int main(void) {
     int t;
     scanf("%d", &t);
 
-    for (int tc = 0; tc < t; tc++) {
-        int k;
-        scanf("%d", &k);
+    for (int i = 0; i < t; i++) {
+        int q;
+        scanf("%d", &q);
 
-        db_queue dq;
-        init_queue(&dq, k);
+        heap min_heap, max_heap;
+        init_heap(min_heap, q);
+        init_heap(max_heap, q);
 
-        for (int i = 0; i < k; i++) {
-            char func;
-            int value;
-            scanf("%c %d", &func, &value);
+        deletion_map d_map;
+        init_map(d_map);
+
+        for (int j = 0; j < q; j++) {
+            char func; int num;
+            scanf(" %c %d", &func, &num);
 
             if (func == 'I') {
-                insert(&dq, value);
+                insert_min(min_heap, num);
+                insert_max(max_heap, num);
+
             } else if (func == 'D') {
-                if (value == 1) {
-                    delete_max(&dq);
-                } else if (value == -1) {
-                    delete_min(&dq);
+                if (num == 1) {
+
+                } else if (num == -1) {
+
                 } else return 1;
             } else return 1;
         }
-
-        if (dq.rear <= 0) printf("EMPTY\n");
-        else {
-            if (dq.rear == 1) {
-                int n = delete_min(&dq);
-                printf("%d %d", n, n);
-            }
-            else printf("%d %d\n", delete_min(&dq), delete_max(&dq));
-        }
-
-        free_queue(&dq);
     }
 
     return 0;
--- Silver/1874.c
+++ Silver/1874.c
@@ -1,5 +1,5 @@
 /* 백준 1874(스택 수열)
- */
+스택을 구현한 후에 스택의 top과 배열의 값을 비교해가며 push, pop 시행 */
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -22,7 +22,7 @@ void init_stack (stack* s, int size) {
 }
 
 bool is_empty (stack* s) {
-    return s->size == -1;
+    return s->top == -1;
 }
 
 bool is_full (stack* s) {
@@ -39,7 +39,7 @@ void push (stack* s, int value) {
 }
 
 int pop (stack* s) {
-    if (!is_empty) return s->arr[s->top--];
+    if (!is_empty(s)) return s->arr[s->top--];
     else {
         printf("stack is empty\n");
         return 0;
@@ -58,12 +58,47 @@ int main(void) {
 
     init_stack(&s, n);
     int* arr = malloc(n * sizeof(int));
+    char* res = malloc(n * 2 * sizeof(char));
 
     for (int i = 0; i < n; i++) {
         scanf("%d", &arr[i]);
     }
 
-    
+    int r_index = 0;
+    push(&s, 1);
+    res[r_index++] = '+';
+
+    int push_num = 2, fail = 0;
+    for (int i = 0; i < n; i++) {   //수의 순서를 저장한 배열을 따라가는 for문
+        if (s.arr[s.top] == arr[i]) {
+            pop(&s);
+            res[r_index++] = '-';
+        } else if (s.arr[s.top] < arr[i]) {
+            while (s.arr[s.top] < arr[i]) {
+                push(&s, push_num++);
+                res[r_index++] = '+';
+            }
+
+            if (s.arr[s.top] == arr[i]) {
+                pop(&s);
+                res[r_index++] = '-';
+            }
+        } else {    //push나 pop 둘 중 아무것도 시행되지 않았다면 불가능한 것이므로 중단
+            fail = 1;
+            break;
+        }
+    }
+
+    if (fail == 1) printf("NO\n");
+    else {
+        for (int i = 0; i < r_index; i++) {
+            printf("%c\n", res[i]);
+        }
+    }
+
+    free_stack(&s);
+    free(arr);
+    free(res);
 
     return 0;
 }
